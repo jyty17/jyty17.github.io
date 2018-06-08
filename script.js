78af71849590ca8dfc6e27d2d86e2b59e7ab4696
jyty17 <jerryyeh17@gmail.com>
2018-06-07 21:53:15 -0400

    Resized content section

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function myFunction() {
 	var x = document.getElementById("floattext");
 	if (x.style.display === "none" ) {
 		x.style.display = "block";
-    console.log("block");
 	} else {
 		x.style.display = "none";
-    console.log("none");
 	}
 	return true;
 };
