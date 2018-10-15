c2a1d89d126d69dd4b41b8af28e6f1e9de28b7f4
jyty17 <jerryyeh17@gmail.com>
2018-10-15 15:48:09 -0400

    minor changes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function myFunction() {
 	var x = document.getElementById("floattext");
+	var y = document.getElementById("stonehenge");
 	if (x.style.display === "none" ) {
 		x.style.display = "block";
+		y.style.display = "none"
 	} else {
 		x.style.display = "none";
+		y.style.display = "block";
 	}
+	// document.body.style.backgroundColor = "#F1F0FF";
 	return true;
 };
